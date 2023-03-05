619b929866cddf76b56669513fa58d6c19eb37fc
Salvador Ahumada <ahumada1790@gmail.com>
2023-03-04 23:32:21 -0600

    feat: Improved logic, added validation for project names

--- init.bat
+++ init.bat
@@ -1,24 +1,35 @@
-@ECHO OFF
+@echo OFF
 
-SET foo=%1
-
-IF "%foo%"=="" (
-    
-    ECHO usage: init [proyect]
-    ECHO proyects:
-    ECHO goa10 - a100d
-    
-    ) else (
-        IF "%foo%"=="goa10" (
-            cd ""C:\Users\lsaz1\Documents\WEBDEV\a100d && code . && wt -w 0 -d C:\Users\lsaz1\Documents\WEBDEV\a100d""
-            )
-    )
+SET project=%1
 
 
+if not exist C:\Users\%USERNAME%\Documents\WEBDEV\ (
+  ECHO [7;31mFolder C:\Users\%USERNAME%\Documents\WEBDEV\ Not Found[0m   
+  exit /b
+) 
 
-REM cd ""C:\Users\lsaz1\Documents\WEBDEV\a100d""
+if not exist C:\Users\%USERNAME%\Documents\WEBDEV\%project% (
+  ECHO [7;31mC:\Users\%USERNAME%\Documents\WEBDEV\%1 Not Found[0m
+  exit /b
+) 
 
+if "%project%"=="" (
+    ECHO [Run-Init V0.1]
+    ECHO usage: init [project]
+    ECHO projects:
+    ECHO a100d
+    EXIT /B %ERRORLEVEL%
+)
 
-REM start code "c:\scripts\init.bat"
+CALL :Main %project%
 
+EXIT /B %ERRORLEVEL%
+
+:Main
+    GOTO :Start_Project %1
+exit /b
+
+:Start_Project
+    ECHO [7mOpening %1...[0m
+    cd C:\Users\%USERNAME%\Documents\WEBDEV\%1 && code . && wt -w 0 -d C:\Users\%USERNAME%\Documents\WEBDEV\%1 && ECHO [7mDone![0m
 exit /b
